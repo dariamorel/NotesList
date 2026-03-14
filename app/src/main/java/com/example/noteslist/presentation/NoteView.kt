8ed648a42a5e83b98a9b52716b16715ce0f29379
Daria Morel <moreld.m.2005@mail.ru>
2026-03-14 22:58:19 +0300

    Добавлена галочка

--- app/src/main/java/com/example/noteslist/presentation/NoteView.kt
+++ app/src/main/java/com/example/noteslist/presentation/NoteView.kt
@@ -37,10 +37,13 @@ class NoteView @JvmOverloads constructor(
     private val defaultElevation = 0f
     private val titleRectColor = "#62abf0".toColorInt()
     private val starColor = "#ebc12a".toColorInt()
+    private val checkCircleColor = "#40962a".toColorInt()
+    private val checkColor = Color.WHITE
 
     // Инициализируются в init
     private var titleRectHeight = 0f
     private var starSize = 0f
+    private var checkSize = 0f
     private var titleSize = 0f
     private var bodySize = 0f
     private var timeSize = 0f
@@ -53,6 +56,7 @@ class NoteView @JvmOverloads constructor(
     private val titleFrameRect = RectF()
     private val titleFrameStroke = RectF()
     private val starRect = RectF()
+    private val checkRect = RectF()
 
     // Стейт
     private var cornerRadius = defaultCornerRadius
@@ -67,6 +71,8 @@ class NoteView @JvmOverloads constructor(
     private val titlePaint = TextPaint().apply { isAntiAlias = true }
     private val bodyPaint = TextPaint().apply { isAntiAlias = true }
     private val timePaint = TextPaint().apply { isAntiAlias = true }
+    private val checkCirclePaint = Paint().apply { isAntiAlias = true }
+    private val checkPaint = TextPaint().apply { isAntiAlias = true }
     private lateinit var bodyLayout: StaticLayout
 
     init {
@@ -118,6 +124,15 @@ class NoteView @JvmOverloads constructor(
             typeface = Typeface.DEFAULT
             color = textColor
         }
+        checkCirclePaint.apply {
+            style = Paint.Style.FILL
+            color = checkCircleColor
+        }
+        checkPaint.apply {
+            textSize = checkSize - 6
+            typeface = Typeface.DEFAULT
+            color = checkColor
+        }
     }
 
     private fun initDimensions() {
@@ -125,6 +140,7 @@ class NoteView @JvmOverloads constructor(
         resources.apply {
             titleRectHeight = getDimension(R.dimen.title_rect_height)
             starSize = getDimension(R.dimen.star_size)
+            checkSize = getDimension(R.dimen.check_size)
             titleSize = getDimension(R.dimen.title_size)
             bodySize = getDimension(R.dimen.body_size)
             timeSize = getDimension(R.dimen.time_size)
@@ -163,6 +179,12 @@ class NoteView @JvmOverloads constructor(
             titleFrameRect.top + textVerticalPadding,
             titleFrameRect.left + textHorizontalPadding + starSize,
             titleFrameRect.top + textVerticalPadding + starSize
+        )
+        checkRect.set(
+            frameRect.right - textHorizontalPadding - checkSize,
+            frameRect.bottom - textVerticalPadding - checkSize,
+            frameRect.right - textHorizontalPadding,
+            frameRect.bottom - textVerticalPadding
         )
 
         val availableBodyWidth = (frameRect.right - frameRect.left - textHorizontalPadding * 2).toInt()
@@ -208,5 +230,11 @@ class NoteView @JvmOverloads constructor(
         val timeBaseline = frameRect.bottom - textVerticalPadding
         val createTime = note?.getCreateTimeFormatted() ?: ""
         canvas.drawText(createTime, timeX, timeBaseline, timePaint)
+
+        // Круг с галочкой
+        canvas.drawCircle(checkRect.centerX(), checkRect.centerY(), checkSize / 2, checkCirclePaint)
+        val checkX = checkRect.centerX() - checkSize / 3
+        val checkBaseline = checkRect.bottom - checkSize / 4
+        canvas.drawText("✓", checkX, checkBaseline, checkPaint)
     }
 }
